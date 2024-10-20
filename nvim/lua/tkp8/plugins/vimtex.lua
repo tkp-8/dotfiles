46c571e71f661575098250cc0c9ff2d76bcafc25
tkp-8 <tomkpilgram@icloud.com>
2024-10-20 10:13:57 +0900

    vimtex config

--- nvim/lua/tkp8/plugins/vimtex.lua
+++ nvim/lua/tkp8/plugins/vimtex.lua
@@ -4,5 +4,9 @@ return {
   "lervag/vimtex",
   lazy = false,
   init = function()
+    vim.g.tex_flavor = 'latex'
+    vim.g.vitmex_quickfix_mode = 0
+    vim.conceallevel = 1
+    vim.g.tex_conceal = 'abdmg'
   end,
 }
